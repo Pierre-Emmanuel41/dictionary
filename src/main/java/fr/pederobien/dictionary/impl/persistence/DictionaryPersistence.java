d0c5aef8ed9f8968d5fa10710b91722b01269736
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-16 13:53:43 +0200

    Updating project du to update in project fr.pederobien.persistence

--- src/main/java/fr/pederobien/dictionary/impl/persistence/DictionaryPersistence.java
+++ src/main/java/fr/pederobien/dictionary/impl/persistence/DictionaryPersistence.java
@@ -1,6 +1,9 @@
 package fr.pederobien.dictionary.impl.persistence;
 
+import java.io.IOException;
 import java.nio.file.Path;
+
+import org.w3c.dom.Document;
 
 import fr.pederobien.dictionary.interfaces.IDictionary;
 import fr.pederobien.persistence.impl.xml.AbstractXmlPersistence;
@@ -10,6 +13,11 @@ public class DictionaryPersistence extends AbstractXmlPersistence<IDictionary> {
 	public DictionaryPersistence(Path path) {
 		super(path);
 		register(new DictionaryPersistenceLoader());
+	}
+
+	@Override
+	protected Document createDoc(Object... objects) throws IOException {
+		return parseFromFileName((String) objects[0]);
 	}
 
 	@Override
